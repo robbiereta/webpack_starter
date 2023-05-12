cc2a5874c6baebaab2d26104176882e74d4cdc85
robbiereta <robbie.reta67@gmail.com>
2023-05-12 23:02:18 +0000

    .

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,20 @@ const path = require('path');
  const HtmlWebpackPlugin = require('html-webpack-plugin');
 
  module.exports = {
+  resolve: {
+    fallback: {
+      "fs": false,
+      "tls": false,
+      "net": false,
+      "path": false,
+      "zlib": false,
+      "http": false,
+      "https": false,
+      "stream": false,
+      "crypto": false,
+      "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify 
+    }
+    } ,
    mode: 'development',
    entry: {
      index: './src/index.js',
