7d9c95bd38ccca7ecbb8c428a58dbdace148ae00
robbiereta <robbie.reta67@gmail.com>
2023-05-17 22:08:54 +0000

    .

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import { sendEmailFromApi } from "./mailer";
 export default function Hello() {
-  
+
+  sendEmailFromApi()
+    
     return (
     <div>Hello</div>
   )
