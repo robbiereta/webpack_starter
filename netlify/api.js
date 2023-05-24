6000c71867a91f472a80324e56f284dddca23fdf
robbiereta <robbie.reta67@gmail.com>
2023-05-24 20:02:21 +0000

    .

--- netlify/api.js
+++ netlify/api.js
@@ -1,12 +1,15 @@
-exports.handler = async function (event, context) {
-  const express = require('express')
+import express, { Router } from 'express';
+
+export async function handler(event, context) {
+
   const app = express()
   const port = 3000
   
   var mailerRouter = require("./mailer");
-  
-  app.use('/mailer',mailerRouter)
-  
+
+  const router = Router();
+  router.get('/hello', (req, res) => res.send('Hello World!'));
+  app.use('/api/', router);
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
