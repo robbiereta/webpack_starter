b6f500441b5947a0a5e32a3ec33976591831d5f0
robbie <robbie.reta67@gmail.com>
2023-05-31 16:19:10 -0600

    .

--- functions/serverlessHttp.js
+++
@@ -1,7 +0,0 @@
-import serverless from "serverless-http";
-import expressApp from "./api";
-
-const functionName = "serverlessHttp"
-const app = expressApp(functionName);
-
-exports.handler= serverless(app);
--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
-//import { sendEmailFromApi } from "./mailer";
-export default function Hello() {    
-    return (
-    <div>Hello</div>
+import { useForm } from "react-hook-form";
+import Select from 'react-select'
+function Hello() {
+ 
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+  const opciones = [{
+    label: 'Seleccione una opción',
+    value: ''
+  },
+  {
+    label: 'ft125',
+    value: 'ft125'
+  }
+]
+
+  return (
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)}>
+    <Select options={opciones} />
+  <div class="form-outline mb-4">
+    <input type="text" id="form1Example1" class="form-control" {...register("nombre",{required:true})}/>
+    <label class="form-label" for="form1Example1">Nombre</label>
+  </div>
+
+  <div class="form-outline mb-4">
+  <input type="text" id="form1Example2" class="form-control" {...register("telefono",{required:true})}/>
+  <label class="form-label" for="form1Example2">Telefono</label>
+</div>
+
+      {errors.exampleRequired && <span>This field is required</span>}
+      <button type="submit" class="btn btn-primary btn-block">Solicitar cotizacion</button>
+
+    </form>
+    
   )
-  
 }
+
+export default Hello
+
