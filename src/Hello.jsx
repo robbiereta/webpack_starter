125e0ff2f7b12ec7abcadca3c7849abee0c19f93
robbie <robbie.reta67@gmail.com>
2023-06-02 12:36:50 -0600

    .

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,32 +1,59 @@
 import React from 'react'
 import { useForm } from "react-hook-form";
 import Select from 'react-select'
+import { grupo,prods,anios } from "./opciones";
 function Hello() {
  
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
   const onSubmit = data => console.log(data);
-  const opciones = [{
-    label: 'Seleccione una opción',
-    value: ''
-  },
-  {
-    label: 'ft125',
-    value: 'ft125'
-  }
-]
 
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)}>
-    <Select options={opciones} />
-  <div class="form-outline mb-4">
-    <input type="text" id="form1Example1" class="form-control" {...register("nombre",{required:true})}/>
-    <label class="form-label" for="form1Example1">Nombre</label>
+    <div class="row d-flex justify-content-center">
+    
+      <div class="col-lg-3">
+      <h2>Cotizaciones Bici-Vic</h2>
+      </div>
+    
+    </div>
+  
+    <hr />
+    <h4>Selecciona un producto:</h4>
+    <Select
+      options={prods}
+      onChange={data => console.log(data)}
+      placeholder="Selecciona un producto"
+      name="producto" />
+    <h4>Selecciona una moto:</h4>
+    <Select
+      options={grupo}
+      onChange={data => console.log(data)}
+      placeholder="Selecciona una moto"
+      name="moto"></Select>
+      <h4>Selecciona el año:</h4>
+      <Select
+        options={anios}
+        onChange={data => console.log(data)}
+        placeholder="Selecciona el año"
+        name="anio"></Select>
+
+    <div class="form-outline mb-4">
+    <input type="text" id="form1Example3" class="form-control" {...register("color",{required:true})}/>
+    <label class="form-label" for="form1Example3"> Ingresa el color de tu  moto </label>
   </div>
 
+
+
+    <div class="form-outline mb-4">
+    <input type="text" id="form1Example1" class="form-control" {...register("nombre",{required:true})}/>
+    <label class="form-label" for="form1Example1"> Ingresa tu Nombre</label>
+    </div>
+
+  
   <div class="form-outline mb-4">
   <input type="text" id="form1Example2" class="form-control" {...register("telefono",{required:true})}/>
-  <label class="form-label" for="form1Example2">Telefono</label>
+  <label class="form-label" for="form1Example2">  Ingresa tu Telefono</label>
 </div>
 
       {errors.exampleRequired && <span>This field is required</span>}
---
+++ src/opciones.js
@@ -0,0 +1,132 @@
+
+
+export const italika= [
+    {"label":"FT-125","value":"FT-125"},{"label":"FT-150","value":"FT-150"},{"label":"FT-250","value":"FT-250"},{"label":"FT-110","value":"FT-110"},{"label":"FT-115","value":"FT-115"},{"label":"FT-125TS","value":"FT-125TS"},{"label":"FT-150TS","value":"FT-150TS"},{"label":"FT-180TS","value":"FT-180TS"},{"label":"FT-200TS","value":"FT-200TS"},{"label":"FT-250TS","value":"FT-250TS"},{"label":"125Z","value":"125Z"},{"label":"150Z","value":"150Z"},{"label":"170Z","value":"170Z"},{"label":"200Z","value":"200Z"},{"label":"250Z","value":"250Z"},{"label":"DM-125","value":"DM-125"},{"label":"DM-150","value":"DM-150"},{"label":"DM-200","value":"DM-200"},{"label":"DM-250","value":"DM-250"},{"label":"DM-250X","value":"DM-250X"},{"label":"DM-150 ROJA","value":"DM-150 ROJA"},{"label":"DS-125","value":"DS-125"},{"label":"DS150","value":"DS150"},{"label":"GTS175","value":"GTS175"},{"label":"AT110","value":"AT110"},{"label":"AT125","value":"AT125"},{"label":"RC150","value":"RC150"},{"label":"RC200","value":"RC200"},{"label":"TC200","value":"TC200"},{"label":"TC250","value":"TC250"},{"label":"RT180","value":"RT180"},{"label":"RT200","value":"RT200"},{"label":"RT250","value":"RT250"},{"label":"V200","value":"V200"},{"label":"VORTX200","value":"VORTX200"}
+]
+
+
+export const honda = [{
+label: 'cgl125',
+value: 'cgl125'
+},
+{
+    label: 'gl150',
+    value: 'gl150'
+},
+{
+    label: 'cargo125 viejita',
+    value: 'cargo125 viejita' 
+},
+{
+    label: 'xr150',
+    value: 'xr150'
+}, 
+{
+    label: 'xr250',
+    value: 'xr250'
+},
+]
+
+export const yamaha = [{
+    label: 'ybr125',
+    value: 'ybr125'
+}]
+
+export const suzuki = [{
+    label: 'en125',
+    value: 'en125'
+    
+},
+{
+    label: 'ax4',
+    value: 'ax4'
+    
+},
+{
+    label: 'ax100',
+    value: 'ax100'
+    
+}]
+
+export const prods = [
+{"label":"ACEITE","value":"ACEITE"},{"label":"AJUSTADOR DE CADENA","value":"AJUSTADOR DE CADENA"},{"label":"ALFORJAS","value":"ALFORJAS"},{"label":"AMORTIGUADOR","value":"AMORTIGUADOR"},{"label":"ANILLOS","value":"ANILLOS"},{"label":"ARAÑA","value":"ARAÑA"},{"label":"ARBOL DE LEVAS","value":"ARBOL DE LEVAS"},{"label":"ARO","value":"ARO"},{"label":"ASIENTO","value":"ASIENTO"},{"label":"AUMENTO DE AMORTIGUADOR","value":"AUMENTO DE AMORTIGUADOR"},{"label":"BALANCIN","value":"BALANCIN"},{"label":"BALATAS","value":"BALATAS"},{"label":"BALERO","value":"BALERO"},{"label":"BANDA MOTONETA","value":"BANDA MOTONETA"},{"label":"BARRA DE SUSPENSION","value":"BARRA DE SUSPENSION"},{"label":"BASE DE YUGO DE DIRECCION","value":"BASE DE YUGO DE DIRECCION"},{"label":"BATERIA","value":"BATERIA"},{"label":"BAYONETA","value":"BAYONETA"},{"label":"BENDIX","value":"BENDIX"},{"label":"BIELA","value":"BIELA"},{"label":"BOMBA DE ACEITE","value":"BOMBA DE ACEITE"},{"label":"BOMBA DE FRENO","value":"BOMBA DE FRENO"},{"label":"BUJES DE MAZA","value":"BUJES DE MAZA"},{"label":"BUJIA","value":"BUJIA"},{"label":"CABEZA","value":"CABEZA"},{"label":"CABLE DE ACELERADOR","value":"CABLE DE ACELERADOR"},{"label":"CABLE DE AHOGADOR","value":"CABLE DE AHOGADOR"},{"label":"CABLE DE BUJIA","value":"CABLE DE BUJIA"},{"label":"CABLE DE CLUTCH","value":"CABLE DE CLUTCH"},{"label":"CABLE DE FRENOS","value":"CABLE DE FRENOS"},{"label":"CABLE DE TACOMETRO","value":"CABLE DE TACOMETRO"},{"label":"CABLE DE VELOCIMETRO","value":"CABLE DE VELOCIMETRO"},{"label":"CABLEADO ARNES","value":"CABLEADO ARNES"},{"label":"CADENA DE TIEMPO","value":"CADENA DE TIEMPO"},{"label":"CALAVERA STOP","value":"CALAVERA STOP"},{"label":"CAMARA","value":"CAMARA"},{"label":"CANDADO DE CADENA","value":"CANDADO DE CADENA"},{"label":"CAPUCHONES","value":"CAPUCHONES"},{"label":"CARBONES","value":"CARBONES"},{"label":"CARBUKLIN","value":"CARBUKLIN"},{"label":"CARBURADOR","value":"CARBURADOR"},{"label":"CARTER","value":"CARTER"},{"label":"CASCO","value":"CASCO"},{"label":"CDI","value":"CDI"},{"label":"CEDAZO","value":"CEDAZO"},{"label":"CEMENTO","value":"CEMENTO"},{"label":"CIGÜEÑAL","value":"CIGÜEÑAL"},{"label":"CLAXON MOTO","value":"CLAXON MOTO"},{"label":"CLUTCH COMPLETO","value":"CLUTCH COMPLETO"},{"label":"CONTRAPESO","value":"CONTRAPESO"},{"label":"CORBATA","value":"CORBATA"},{"label":"CUBIERTA","value":"CUBIERTA"},{"label":"CUBIERTA ENFRIAMIENTO","value":"CUBIERTA ENFRIAMIENTO"},{"label":"CUBREPUÑOS","value":"CUBREPUÑOS"},{"label":"CUÑA","value":"CUÑA"},{"label":"DEFENSA","value":"DEFENSA"},{"label":"DIAFRAGMA","value":"DIAFRAGMA"},{"label":"DIRECCIONAL DE MOTOCICLETA","value":"DIRECCIONAL DE MOTOCICLETA"},{"label":"EJE CAMBIOS","value":"EJE CAMBIOS"},{"label":"EMPAQUES","value":"EMPAQUES"},{"label":"ESCAPE","value":"ESCAPE"},{"label":"ESPEJOS DE MOTOCICLETA","value":"ESPEJOS DE MOTOCICLETA"},{"label":"ESTATOR","value":"ESTATOR"},{"label":"FARO DE MOTOCICLETA","value":"FARO DE MOTOCICLETA"},{"label":"FILTRO DE AIRE","value":"FILTRO DE AIRE"},{"label":"FILTRO DE GASOLINA","value":"FILTRO DE GASOLINA"},{"label":"FOCO","value":"FOCO"},{"label":"FUSIBLE","value":"FUSIBLE"},{"label":"GALLETA MOFLE","value":"GALLETA MOFLE"},{"label":"GOMAS SPROCK","value":"GOMAS SPROCK"},{"label":"GUANTES","value":"GUANTES"},{"label":"GUIAS","value":"GUIAS"},{"label":"HUESITO","value":"HUESITO"},{"label":"IMPERMEABLE COMPLETO","value":"IMPERMEABLE COMPLETO"},{"label":"KIT CILINDRO","value":"KIT CILINDRO"},{"label":"KIT DE TAZAS DE DIRECCION","value":"KIT DE TAZAS DE DIRECCION"},{"label":"KIT SPROCKET","value":"KIT SPROCKET"},{"label":"LENTES","value":"LENTES"},{"label":"LEVA","value":"LEVA"},{"label":"LIGA","value":"LIGA"},{"label":"LINEA DE FRENO","value":"LINEA DE FRENO"},{"label":"LLANTA","value":"LLANTA"},{"label":"LLAVE DE GASOLINA","value":"LLAVE DE GASOLINA"},{"label":"LUBRICANTE CADENA","value":"LUBRICANTE CADENA"},{"label":"MALETERO DE MOTOCICLETA","value":"MALETERO DE MOTOCICLETA"},{"label":"MALLA DE MOTOCICLETA","value":"MALLA DE MOTOCICLETA"},{"label":"MANDO","value":"MANDO"},{"label":"MANGUERA DE CARBURADOR","value":"MANGUERA DE CARBURADOR"},{"label":"MANIJA DE CLUTCH","value":"MANIJA DE CLUTCH"},{"label":"MANIJA DE FRENO","value":"MANIJA DE FRENO"},{"label":"MANUBRIO","value":"MANUBRIO"},{"label":"MARCHA","value":"MARCHA"},{"label":"MAZA TRASERA","value":"MAZA TRASERA"},{"label":"MEDIA LUNA","value":"MEDIA LUNA"},{"label":"MICA DE CASCO","value":"MICA DE CASCO"},{"label":"MONITO","value":"MONITO"},{"label":"OPRESOR","value":"OPRESOR"},{"label":"PALANCA DERECHA","value":"PALANCA DERECHA"},{"label":"PARADOR LATERAL","value":"PARADOR LATERAL"},{"label":"PARRILLA","value":"PARRILLA"},{"label":"PASTA DE CLUTCH","value":"PASTA DE CLUTCH"},{"label":"PEDAL DE ARRANQUE","value":"PEDAL DE ARRANQUE"},{"label":"PEDAL DE CAMBIOS","value":"PEDAL DE CAMBIOS"},{"label":"PEDAL DE FRENOS","value":"PEDAL DE FRENOS"},{"label":"PIPETA","value":"PIPETA"},{"label":"PISTON","value":"PISTON"},{"label":"PIVOTE","value":"PIVOTE"},{"label":"PLATO CLUTCH","value":"PLATO CLUTCH"},{"label":"POLEA","value":"POLEA"},{"label":"PORTABALATAS","value":"PORTABALATAS"},{"label":"PORTACELULAR","value":"PORTACELULAR"},{"label":"PORTAENGRANE","value":"PORTAENGRANE"},{"label":"POSAPIE","value":"POSAPIE"},{"label":"PRENSA CLUTCH","value":"PRENSA CLUTCH"},{"label":"PUÑOS DE MOTO","value":"PUÑOS DE MOTO"},{"label":"RAYO DE MOTO","value":"RAYO DE MOTO"},{"label":"REFLEJANTE","value":"REFLEJANTE"},{"label":"REGULADOR","value":"REGULADOR"},{"label":"RELAY DIRECCIONALES","value":"RELAY DIRECCIONALES"},{"label":"REPUESTO DE CARBURADOR","value":"REPUESTO DE CARBURADOR"},{"label":"RESORTE","value":"RESORTE"},{"label":"RETENES","value":"RETENES"},{"label":"RIN DE MOTO","value":"RIN DE MOTO"},{"label":"RODILLOS CLUTCH","value":"RODILLOS CLUTCH"},{"label":"SALPICADERA","value":"SALPICADERA"},{"label":"SENSOR DE STOP","value":"SENSOR DE STOP"},{"label":"SEPARADOR PIPETA","value":"SEPARADOR PIPETA"},{"label":"SILICON DE MOTOCICLETA","value":"SILICON DE MOTOCICLETA"},{"label":"SOCKET PARA FARO","value":"SOCKET PARA FARO"},{"label":"SPROCK TRASERO","value":"SPROCK TRASERO"},{"label":"SPROCKET DELANTERO","value":"SPROCKET DELANTERO"},{"label":"SUJETADOR DE ESPEJOS","value":"SUJETADOR DE ESPEJOS"},{"label":"SWITCH","value":"SWITCH"},{"label":"TABLERO","value":"TABLERO"},{"label":"TANQUE DE GASOLINA","value":"TANQUE DE GASOLINA"},{"label":"TAPON DE MOTOCICLETA","value":"TAPON DE MOTOCICLETA"},{"label":"TIRA DE LEDS","value":"TIRA DE LEDS"},{"label":"TORNILLOS SPROCK","value":"TORNILLOS SPROCK"},{"label":"VALVULAS","value":"VALVULAS"},{"label":"VARILLA DE FRENO","value":"VARILLA DE FRENO"},{"label":"YUGO DE DIRECCION","value":"YUGO DE DIRECCION"},{"label":"ZOQUETERA","value":"ZOQUETERA"}
+
+]
+
+export const anios = [
+    {
+        label: '2010',
+        value: 2010
+    },
+    {
+        label: '2011',
+        value: 2011
+    },
+    {
+        label: '2012',
+        value: 2012
+    },
+    {
+        label: '2013',
+        value: 2013
+    },
+    {
+        label: '2014',
+        value: 2014
+    },
+    {
+        label: '2015',
+        value: 2015
+    },
+    {
+        label: '2016',
+        value: 2016
+    },
+    {
+        label: '2017',
+        value: 2017
+    },
+    {
+        label: '2018',
+        value: 2018
+    },
+    {
+        label: '2019',
+        value: 2019
+    },
+    {
+        label: '2020',
+        value: 2020
+    },
+    {
+        label: '2021',
+        value: 2021
+    },
+    {
+        label: '2022',
+        value: 2022
+    },
+    {
+        label: '2023',
+        value: 2023
+    }
+]
+export const grupo = [
+   
+    {
+        label: 'Italika',
+        options: italika
+    },
+    {
+        label: 'Honda',
+        options: honda
+    },
+    {
+        label: 'Yamaha',
+        options: yamaha
+    },
+    {
+        label: 'Suzuki',
+        options: suzuki
+    },
+]
